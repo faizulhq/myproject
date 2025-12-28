d85474db5562147372c02efcd3949bfcfea0468f
Faiuzlhaq <faizulhaq1011@gmail.com>
2025-12-28 10:35:17 +0700

    Refactor ItemSerializer to use automatic URLs
    Removed manual image_url and document_url fields from ItemSerializer. Updated fields to use automatic URLs from Cloudinary.

--- items/serializers.py
+++ items/serializers.py
@@ -2,33 +2,17 @@ from rest_framework import serializers
 from .models import Item
 
 class ItemSerializer(serializers.ModelSerializer):
-    image_url = serializers.SerializerMethodField()
-    document_url = serializers.SerializerMethodField()
-    
     class Meta:
         model = Item
+        # Kita hapus image_url dan document_url buatan manual.
+        # Field 'image' dan 'document' dari Cloudinary sudah otomatis berupa URL lengkap.
         fields = [
             'id', 
             'name', 
             'description', 
-            'image',           
-            'document',        
-            'image_url',       
-            'document_url',    
+            'image',     # Ini akan otomatis jadi URL (https://res.cloudinary...)
+            'document',  # Ini juga otomatis jadi URL
             'created_at', 
             'updated_at'
         ]
-    
-    def get_image_url(self, obj):
-        if obj.image:
-            request = self.context.get('request')
-            if request:
-                return request.build_absolute_uri(obj.image.url)
-        return None
-    
-    def get_document_url(self, obj):
-        if obj.document:
-            request = self.context.get('request')
-            if request:
-                return request.build_absolute_uri(obj.document.url)
-        return None
+
