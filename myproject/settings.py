c876b8b1d89a448441c47c659cb372cd5bd70a82
faizulhq <faizulhaq1011@gmail.com>
2026-01-03 19:23:01 +0700

    Add admin settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -222,6 +222,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    # Menambahkan field 'role' dan 'avatar' ke fieldsets (Edit Form)
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Info', {'fields': ('role', 'avatar')}),
+    )
+    # Menambahkan ke add_fieldsets (Create Form)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Custom Info', {'fields': ('role', 'avatar')}),
+    )
+    # Menampilkan kolom role di list user
+    list_display = ['username', 'email', 'role', 'is_staff']
+    list_filter = ['role', 'is_staff', 'is_superuser']
+
+admin.site.register(User, CustomUserAdmin)
