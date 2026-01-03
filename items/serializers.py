ecd4c64b0bad7b388dd2ad09abcf6cd44b1e0318
faizulhq <faizulhaq1011@gmail.com>
2026-01-03 11:40:38 +0700

    Modified serializers

--- items/serializers.py
+++ items/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from .models import Item
 
 class ItemSerializer(serializers.ModelSerializer):
+    # Force absolute URL untuk image dan document
+    image = serializers.SerializerMethodField()
+    document = serializers.SerializerMethodField()
+    
     class Meta:
         model = Item
         fields = [
@@ -13,4 +17,14 @@ class ItemSerializer(serializers.ModelSerializer):
             'created_at', 
             'updated_at'
         ]
-
+    
+    def get_image(self, obj):
+        if obj.image:
+            # CloudinaryField.url sudah return full URL
+            return str(obj.image.url) if hasattr(obj.image, 'url') else str(obj.image)
+        return None
+    
+    def get_document(self, obj):
+        if obj.document:
+            return str(obj.document.url) if hasattr(obj.document, 'url') else str(obj.document)
+        return None
